0db3d24d252a988798612ec4560e947b1c5aa09f
Mahmud-ul Hasan Bari <70122862+Mahmud-ul@users.noreply.github.com>
2024-08-27 01:14:02 +0600

    Add files via upload

---
+++ Ideal.Repository/Contract/ISampleRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ideal.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ideal.Repository.Contract
+{
+    public interface ISampleRepository : IBaseRepository<Sample>
+    {
+    }
+}
---
+++ Ideal.Repository/SampleRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ideal.Model.Models;
+using Ideal.Repository.Base;
+using Ideal.Repository.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ideal.Repository
+{
+    public class SampleRepository : BaseRepository<Sample>, ISampleRepository
+    {
+    }
+}
