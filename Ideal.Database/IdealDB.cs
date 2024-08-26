ba4afad0e0054eaef67ce090d4cd1e8cb1dc2294
Mahmud-ul Hasan Bari <70122862+Mahmud-ul@users.noreply.github.com>
2024-08-27 01:08:14 +0600

    Add files via upload

---
+++ Ideal.Database/IdealDB.cs
@@ -0,0 +1,33 @@
+﻿using Ideal.Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ideal.Database
+{
+    public class IdealDB : DbContext
+    {
+        public DbSet<Sample> Samples { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = @"Data Source = TIPU\SQLEXPRESS;
+                                      Database = IdealDB;
+                                      Trusted_Connection = True;
+                                      Encrypt = False;";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Sample>().HasIndex(b => b.Name).IsUnique();
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+    }
+}
---
+++ Ideal.Database/Migrations/20231116020532_InitialDB.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ideal.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Samples",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Samples", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Samples_Name",
+                table: "Samples",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Samples");
+        }
+    }
+}
